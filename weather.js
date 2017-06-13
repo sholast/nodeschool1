232ee38364c0a45e319ec2bdf91b0b05362e83e0
sholast <kozelalexey@gmail.com>
2017-06-13 16:03:29 +0300

    weather requests

--- weather.js
+++ weather.js
@@ -1,23 +1,47 @@
-
-const requestPromise = require('request-promise');
-const KYIV_URL = 'http://samples.openweathermap.org/data/2.5/forecast?q=Kyiv&appid=181b87b60705ae59e14754992d5f2f48';
-const KHARKIV_URL = 'http://samples.openweathermap.org/data/2.5/forecast?q=Kharkiv&appid=181b87b60705ae59e14754992d5f2f48';
-
-
+const request = require('request');
+const promisify = require("es6-promisify");
+const promisedRequest = promisify(request);
 
 class WeatherRequester {
-    constructor(){
-        this.kharkivWeather = null;
-        this.kyivWeather = null;
+    constructor() {}
+
+    getWeather(city) {
+        const options = {
+            method: "GET",
+            uri: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=181b87b60705ae59e14754992d5f2f48`
+        };
+        return promisedRequest(options);
     }
 
     getWeatherInSeries() {
-
-    }
+        let kyivWeather, kharkivWeather;
+        return this.getWeather('Kyiv')
+            .then(result => {
+                kyivWeather = result.body;
+            })
+            .then(() => {
+                return this.getWeather('Kharkov');
+            })
+            .then(result => {
+                kharkivWeather = result.body;
+                return {kyivWeather, kharkivWeather};
+            })
+            .catch(err => console.error(err));
+    };
 
     getWeatherInParallel() {
-
+        return Promise.all([this.getWeather('Kyiv'), this.getWeather('Kharkiv')])
+            .then(result => {
+                return {
+                    kyivWeather: result[0].body,
+                    kharkivWeather: result[1].body
+                }
+            })
+            .catch(err => console.error(err));
     }
-
-
 }
+
+const weather = new WeatherRequester();
+
+weather.getWeatherInSeries().then(result => console.log(result));
+weather.getWeatherInParallel().then(result => console.log(result));
