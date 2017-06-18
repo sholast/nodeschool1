5a45a3be716b7e60d68acbac79138b2526703677
sholast <kozelalexey@gmail.com>
2017-06-18 13:47:54 +0300

    template engine

--- templater.js
+++ templater.js
@@ -7,47 +7,42 @@ class TemplateTransformer extends Transform {
         this.templateData = options.templateData;
     }
 
-    _transform (chunk, enc, cb) {
+    _transform(chunk, enc, cb) {
         chunk = this.replaceTemplate(chunk.toString());
         this.push(chunk, 'utf8');
         cb();
     };
 
     replaceTemplate(template) {
-        return template.replace(/{{\s*[\w\.]+\s*}}/g, (match =>{
-            let clear = (match.substr(2, match.length -4));
+        return template.replace(/{{\s*[\w\.]+\s*}}/g, (match => {
+            let clear = (match.substr(2, match.length - 4));
             return this.findInTemplateData(clear);
         }));
     }
 
-    findInTemplateData(placeholder){
+    findInTemplateData(placeholder) {
         let data = placeholder.split('.');
-        if(data.length === 1) {
+        if (data.length === 1) {
             return this.templateData[data[0]];
         } else {
-            return placeholder.split('.').reduce((o,i)=>o[i], this.templateData);
+            return placeholder.split('.').reduce((o, i) => o[i], this.templateData);
         }
     }
+}
 
+class TemplateEngine {
+    constructor(template, templateData) {
+        this.readFileStream = fs.createReadStream(template);
+        this.templateTransformer = new TemplateTransformer({
+            templateData: templateData
+        });
+    }
+
+    getStream() {
+        return this.readFileStream.pipe(this.templateTransformer);
+    }
 
 }
 
-const tt = new TemplateTransformer({templateData:  {
-    some: ['111some111', 'asdsd'],
-    other: '222other222',
-    another: '333another333',
-    complex: {
-        comp1: '4444',
-        comp2: '555',
-        comp3: '6666'
-    }
-}});
-
-let readFileStream = fs.createReadStream('template.html');
-
-let writeFileStream = fs.createWriteStream('ready.html');
-
-readFileStream.pipe(tt).pipe(writeFileStream);
-
-
-
+module.exports.TemplateEngine = TemplateEngine;
+module.exports.TemplateTransformer = TemplateTransformer;
